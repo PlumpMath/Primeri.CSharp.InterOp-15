d163ee33618a30806a441a9cef4f157fc2688467
ilnestorova <iliana.nestorova@gmail.com>
2016-10-06 13:10:20 +0300

    Modil9 Video4 AddClssExportToExel

--- Exel/Exel/IOWrite.cs
+++ Exel/Exel/IOWrite.cs
@@ -8,11 +8,46 @@ namespace Exel
 {
     public class IOWrite
     {
-        DataStruct data;
+        private DataStruct data;
        public IOWrite(DataStruct ndata)
         {
-            
+            data = ndata;
         }
-        
+        public bool ExportTable()
+        {
+            try
+            {
+                //mejdinni proverki
+
+
+                return true;
+            } catch { }
+            return false;
+        }
+
+
+        private string GetPath()
+        {
+          return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "table.xlsx");
+        }
+
+
+        public void RunFile()
+        {
+            try
+            {
+
+                System.Diagnostics.Process.Start(GetPath());
+            } catch { }
+        }
+
+
+        public void addrow(DataRow nrow)
+        {
+            try
+            {
+
+            }catch { }
+        }
     }
 }
