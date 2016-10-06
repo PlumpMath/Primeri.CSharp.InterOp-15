7a74591624568b3097deb62080f7a9b8f81bc741
ilnestorova <iliana.nestorova@gmail.com>
2016-10-06 12:44:15 +0300

    Modil9 Video2&3 ArhitectureOfApp&AddDataStructClass

---
+++ Exel/Exel/DataStruct.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exel
+{
+    public   class DataStruct
+    {
+        public List<DataRow> table = new List<DataRow>();
+        public DataStruct()
+        {
+
+        }
+        public void AddRow(string fname, string lname, string age)
+        {
+            table.Add(new DataRow(fname, lname, age));
+        }
+        public void PrintTable()
+        {
+            try
+            {
+                foreach(DataRow row in table)
+                {
+                    Console.WriteLine(row.FirstName+" "+row.LastName+", "+row.Age);
+                }
+
+            }
+            catch { }
+        }
+    }
+    public class DataRow
+    {
+        private string firstName = "";
+        private string lastName = "";
+        private string age = "";
+
+        public DataRow(string nfirstName, string nlastName, string nage)
+        {
+            firstName = nfirstName;
+            lastName = nlastName;
+            age = nage;
+        }
+        public string FirstName
+        {
+            set { value = firstName; }
+            get { return firstName; }
+        }
+        public string LastName
+        {
+            set { value = lastName; }
+            get { return lastName; }
+        }
+        public string Age
+        {
+            set { value = age; }
+            get { return age; }
+        }
+    }
+}
---
+++ Exel/Exel/Exel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exel
+{
+    class Exel
+    {
+        static void Main(string[] args)
+        {
+            DataStruct data = new DataStruct();
+            IOWrite write = new IOWrite(data);
+
+            //zapisvane na danni v osnovnata table
+            data.AddRow("Iliana", "Nestorova", "54");
+            data.AddRow("Gabriela", "Nestorova", "28");
+            data.AddRow("Angel", "Nestorov", "57");
+
+            data.PrintTable();
+        }
+    }
+}
